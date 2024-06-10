dc36a77758b893136f4fb880c7496d795b0e273b
teh83475 <98682804+teh83475@users.noreply.github.com>
2024-06-10 20:45:01 +0800

    Add files via upload

--- react_test/src/App.js
+++ react_test/src/App.js
@@ -4,16 +4,25 @@ import logo from './logo.svg';
 import $ from 'jquery';
 import './App.css';
 
+//do to: maybe remove setPlayingCollection
+
+
 class App extends React.Component {
   constructor(props) {
 		super(props);
 		this.state = {
+      username: "",
 			songs:[],
       collections:[],
-      current:null,
-      current_no:null,
+      current_song:null,
+      current_song_no:-1,
+      playing_collection_no: -1,
+      playing_collection: [],
+      display_collection_no: -1,
+      display_collection: [],
       audio: null,
-      displayPopUp: false
+      displayPopUp: false,
+      song_to_add:""
 		};
   }
 
@@ -21,6 +30,7 @@ class App extends React.Component {
     this.getSong();
     this.getCollection();
 	}
+
 
   getSong(){
     var compo=this;
@@ -32,21 +42,90 @@ class App extends React.Component {
 
   getCollection(){
     var compo=this;
-    $.get('http://localhost:8081/getcollection', function(data) {
+    $.get('http://localhost:8081/getcollection?username='+this.state.username, function(data) {
       //alert(JSON.stringify(data));
-      compo.setState({collections: data})
-    });
+      compo.setState({collections: data})  
+      //-------------not good
+      if (compo.state.playing_collection_no!==-1){
+        var list=[];
+        let i=0;
+        for (i=0;i<compo.state.songs.length;i++){
+          if (data[compo.state.playing_collection_no].list.indexOf(compo.state.songs[i].title) >=0){
+            list.push(i);
+          }
+        }
+        compo.setState({
+          playing_collection: list
+        });
+      }
+      if (compo.state.display_collection_no!==-1){
+        var list=[];
+        let i=0;
+        for (i=0;i<compo.state.songs.length;i++){
+          if (data[compo.state.display_collection_no].list.indexOf(compo.state.songs[i].title) >=0){
+            list.push(i);
+          }
+        }
+        compo.setState({
+          display_collection: list
+        });
+      }
+
+
+    });//.done(compo.setPlayingCollection(compo.state.playing_collection_no));
+    return;
   }
 	
 	componentDidMount() {
 		this.init();
 	}
 
-  setCurrentSong(id){
+  login(name,pw){
+    var compo=this;
+    alert(name+pw);
+    $.post('http://localhost:8081/login',{username:name,password: pw}, function(data) {
+      //alert(JSON.stringify(data));
+      if (data!=="Failed"){
+        
+        compo.setState({
+          username: data
+        }, function(){
+          compo.getCollection();
+        });
+      }
+      alert(data);
+    });
+  }
+
+  logout(){
+    var compo=this;
+    $.get('http://localhost:8081/logout', function(data) {
+      //alert(JSON.stringify(data));
+      compo.setState({
+        username: "",
+        playing_collection_no: -1,
+        playing_collection: [],
+        display_collection_no: -1,
+        display_collection: []
+
+      }, function(){
+        compo.getCollection();
+      });
+      alert(data);
+    });
+  }
+
+  setCurrentSong(id,change_playing_collection){
+    if (change_playing_collection){
+      this.setState({
+        playing_collection_no: this.state.display_collection_no,
+        playing_collection: this.state.display_collection
+      });
+    }
     this.setState({
-      current: this.state.songs[id],
-      current_no: id
-    })
+      current_song: this.state.songs[id],
+      current_song_no: id
+    });
     if (this.state.audio){
       if (!this.state.audio.paused){
         this.state.audio.pause();
@@ -61,31 +140,161 @@ class App extends React.Component {
 
   nextSong() {
 		//alert("next");
-    this.setCurrentSong((this.state.current_no+1)%this.state.songs.length);
+    let next=0;
+    if (this.state.playing_collection_no===-1){
+      next=(this.state.current_song_no+1)%this.state.songs.length;
+    } else {
+      next=this.state.playing_collection[(this.state.playing_collection.indexOf(this.state.current_song_no)+1)%this.state.playing_collection.length];
+    }
+    
+    this.setCurrentSong(next,false);
 	}
 
-  addToCollection(id){
-    this.setState({displayPopUp: true })
+  setPlayingCollection(id){//maybe can remove(change to same as display)
+    //alert(this.state.collections[id].list);
+    
+    console.log(this.state.collections[id]);
+    if (id!==-1){
+      var list=[];
+      let i=0;
+      for (i=0;i<this.state.songs.length;i++){
+        if (this.state.collections[id].list.indexOf(this.state.songs[i].title) >=0){
+          list.push(i);
+          //alert(list);
+        }
+      }
+      this.setState({
+        playing_collection_no: id,
+        playing_collection: list
+      })
+    }
+    
+    
+    
+  }
+
+  setDisplayCollection(id,disableGoBack){
+    if (this.state.display_collection_no===id && !disableGoBack){
+      this.setState({
+        display_collection_no: -1,
+        display_collection: []
+      })
+    }else{
+      var list=[];
+      let i=0;
+      for (i=0;i<this.state.songs.length;i++){
+        if (this.state.collections[id].list.indexOf(this.state.songs[i].title) >=0){
+          list.push(i);
+          //alert(list);
+        }
+      }
+      this.setState({
+        display_collection_no: id,
+        display_collection: list
+      })
+    }
+  }
+
+  addToCollectionShow(id){
+    if (this.state.username!==""){
+      this.setState({
+        displayPopUp: true,
+        song_to_add: this.state.songs[id].title
+      })
+    } else alert("Please login first.")
+    
   }
 
   addToCollectionCancel(){
-    this.setState({displayPopUp: false })
+    this.setState({
+      displayPopUp: false,
+      song_to_add: ""
+    })
   }
+
+  addToCollection(collection_name){//not done
+    if (this.state.username!==""){
+      alert(collection_name);
+      var compo=this;
+      $.post('http://localhost:8081/addtocollection',{username:this.state.username,song: this.state.song_to_add, collection: collection_name}, function(data) {
+        //alert(JSON.stringify(data));
+        alert(data);
+      });
+
+      this.getCollection();
+      this.addToCollectionCancel()
+    } else alert("Please login first.");
+    
+  }
+
+  removeFromCollection(song_no){//not done
+    if (this.state.username!==""){
+      var compo=this;
+      $.post('http://localhost:8081/removefromcollection',{username:this.state.username,song: this.state.songs[song_no].title, collection: this.state.collections[this.state.display_collection_no].name}, function(data) {
+        //alert(JSON.stringify(data));
+        alert(data);
+      });
+
+      this.getCollection();
+    } else alert("Please login first.");
+    
+    
+  }
+
+  newCollection(name){
+    alert(name);
+    
+    if (this.state.username!==""){
+      var compo=this;
+      $.post('http://localhost:8081/newcollection',{username:this.state.username,collection_name: name}, function(data) {
+        //alert(JSON.stringify(data));
+        alert(data);
+      });
+
+      this.getCollection();
+    } else alert("Please login first.");
+    
+  }
+
+  removeCollection(){
+
+    
+    var name=this.state.collections[this.state.display_collection_no].name;
+    alert(name);
+    
+    if (this.state.username!==""){
+      var compo=this;
+      $.post('http://localhost:8081/removecollection',{username:this.state.username,collection_name: name}, function(data) {
+        //alert(JSON.stringify(data));
+        alert(data);
+      });
+      this.setState({
+        playing_collection_no: -1,
+        display_collection_no: -1
+      }, function(){
+        compo.getCollection();
+      });
+      
+    } else alert("Please login first.");
+    
+  }
+
+  
 
   render(){
     return (
       <div className="App">
         <header/>
   
-        <TopNav/>
+        <TopNav login={this.login.bind(this)} logout={this.logout.bind(this)} username={this.state.username}/>
         <div className="main">
-          <SideBar collections={this.state.collections}/>
-          <MainSongSelect songs={this.state.songs} setCurrentSong={this.setCurrentSong.bind(this)} addToCollection={this.addToCollection.bind(this)}/>
+          <SideBar collections={this.state.collections} playing_collection_no={this.state.playing_collection_no} display_collection_no={this.state.display_collection_no} setDisplayCollection={this.setDisplayCollection.bind(this)} setPlayingCollection={this.setPlayingCollection.bind(this)}  newCollection={this.newCollection.bind(this)}/>
+          <MainSongSelect songs={this.state.songs} current_song_no={this.state.current_song_no} display_collection_no={this.state.display_collection_no} display_collection={this.state.display_collection} setCurrentSong={this.setCurrentSong.bind(this)} addToCollectionShow={this.addToCollectionShow.bind(this)} removeFromCollection={this.removeFromCollection.bind(this)} removeCollection={this.removeCollection.bind(this)}/>
         </div>
         
-        <Control current={this.state.current} audio={this.state.audio} nextsong={this.nextSong.bind(this)}/>
+        <Control current_song={this.state.current_song} audio={this.state.audio} nextsong={this.nextSong.bind(this)}/>
         
-        {this.state.displayPopUp? <AddToCollectionPopUp  addToCollectionCancel={this.addToCollectionCancel.bind(this)}/>:""}
+        {this.state.displayPopUp? <AddToCollectionPopUp addToCollection={this.addToCollection.bind(this)}  addToCollectionCancel={this.addToCollectionCancel.bind(this)} collections={this.state.collections}/>:""}
       </div>
     );
   }
@@ -191,27 +400,32 @@ class Control extends React.Component{
 
 
   render(){
-    var songinfo="No song selected"
-    if (this.props.current){
-      songinfo=" "+this.props.current.title+" | "+this.props.current.singer;
+    var title="No song selected";
+    var singer=" ";
+    if (this.props.current_song){
+      title=this.props.current_song.title;
+      singer=this.props.current_song.singer;
     }
     
     return (
-      <div className="col-12">
-        <div className="music-info col-2">
-          &ensp;{songinfo}
+      <div className="col-12 control" >
+        <div className="music-info col-3">
+          &ensp;{title}<br/>
+          &ensp;{singer}
         </div>
 
-        <div className="progress-control col-8">
+        <div className="progress-control col-7">
           <button className="play-pause-button"onClick={()=> this.playPause()}>{this.state.playing? "Pause": "Play"}</button>
           <input type="range" ref={this.progress_slider} min="0" max={this.state.duration} defaultValue="0" className="progress-slider" onChange={this.handleProgressChange}/>
           <div className="progress-display">{convertDuration(this.state.progress)}/{convertDuration(this.state.duration)}</div>
+          <br/>&ensp;
         </div>
 
         <div className="volume-control col-2">
           <div className="volume-title">Volume:</div>
           <input type="range" min="0" max="100" defaultValue="50" className="volume-slider" onChange={this.handleVolumeChange}/>
           <div className="volume-number-display">{this.state.volume}</div>
+          <br/>&ensp;
         </div>
 
         
@@ -225,6 +439,7 @@ class TopNav extends React.Component{
 
     return (
       <div className='top-nav col-12'>
+        {this.props.username!==""? <LogoutBlock logout={this.props.logout}/> :<LoginBlock login={this.props.login}/>}
         
       </div>
     );
@@ -233,16 +448,55 @@ class TopNav extends React.Component{
 
 class LoginBlock extends React.Component{
 
+  constructor(props){
+    super(props);
+    this.state={
+      username:"",
+      password:""
+    };
+  }
 
+  handleUsernameChange(event){
+    this.setState({
+      username: event.target.value
+    });
+  }
+
+  handlePasswordChange(event){
+    this.setState({
+      password: event.target.value
+    });
+  }
 
   render(){
 
     return (
       <div className="login-block" >
+        <div className="input">
+          Username: <input type="text" id="userNameInput"  onChange={this.handleUsernameChange.bind(this)}></input> <br/>
+          Password: <input type="password" id="passwordInput"  onChange={this.handlePasswordChange.bind(this)}></input> 
+        </div>
         
-        Username: <input type="text" id="userNameInput"></input><br/>
-        Password: <input type="password" id="passwordInput"></input><br/>
-        <div><button className="Button" variant="contained" color="primary" >Login</button></div>
+        <button className="Button" onClick={()=>this.props.login(this.state.username,this.state.password)}variant="contained" color="primary" >Login</button>
+      </div>
+    );
+  }
+}
+
+class LogoutBlock extends React.Component{
+
+  constructor(props){
+    super(props);
+
+  }
+
+
+  render(){
+
+    return (
+      <div className="logout-block" >
+        <br/>&ensp;        
+        <button className="Button" onClick={()=>this.props.logout()} color="primary" >Logout</button>
       </div>
     );
   }
@@ -251,18 +505,42 @@ class LoginBlock extends React.Component{
 class SideBar extends React.Component{
   constructor(props){
     super(props);
-    
+    this.state={
+      new_collection_mode:false,
+      new_collection_name:""
+    };
   }
   
+  changeNewCollectionMode(){
+    this.setState({
+      new_collection_mode: !this.state.new_collection_mode
+    });
+  }
 
+  handleNewCollectionNameChange(event){
+    this.setState({
+      new_collection_name: event.target.value
+    });
+  }
 
   render(){
-    var listItems=this.props.collections.map((collection,index) =><li key={collection._id}><CollectionItem no={index} content={collection}/></li>)
+    var listItems=this.props.collections.map((collection,index) =><li key={collection._id}><CollectionItem no={index} content={collection} playing_collection_no={this.props.playing_collection_no} display_collection_no={this.props.display_collection_no} setDisplayCollection={this.props.setDisplayCollection} setPlayingCollection={this.props.setPlayingCollection}/></li>)
     return (
-      <div className="col-2 sidebar">
-        <ul>
+      <div className="col-3 sidebar">
+        <ul className="sidebar-list">
           {listItems}
         </ul>
+        
+        {this.state.new_collection_mode? <div className="sidebar-new-collection">
+            <input type="text" className="sidebar-new-collection-text"onChange={this.handleNewCollectionNameChange.bind(this)}></input>
+            <button onClick={()=>this.props.newCollection(this.state.new_collection_name)}>+</button>
+            <button onClick={()=>this.changeNewCollectionMode()}>x</button>
+          </div>
+        : <button onClick={()=>this.changeNewCollectionMode()}>New</button>}
+          
+          
+          
+        
       </div>
     );
   }
@@ -276,11 +554,25 @@ class CollectionItem extends React.Component{
     
   }
   
+  changeDisplayCollection(event){
+    
+    if(event.target.tagName.toLowerCase()==="div"){
+      this.props.setDisplayCollection(this.props.no,false);
+    }
+  }
+
   render(){
     return (
-      <div className="collection-item">
-        <div className="collection-item-title">&ensp;{this.props.content.name}</div>
-        <div className="collection-item-count">&ensp;No. of songs:&ensp; {this.props.content.list.length}</div>
+      <div className={this.props.display_collection_no===this.props.no?"collection-item selected":"collection-item"} onClick={(e)=>this.changeDisplayCollection(e)}>
+        
+        <div className="collection-iten-info">
+          <div className="collection-item-title">&ensp;{this.props.content.name}</div>
+          <div className="collection-item-count">&ensp;No. of songs:&ensp; {this.props.content.list.length}</div>
+        </div>
+        <div className={this.props.playing_collection_no===this.props.no?"play-icon":"play-icon hidden"}>
+          ▷&ensp;
+        </div>
+        
       </div>
     );
   }
@@ -289,18 +581,25 @@ class CollectionItem extends React.Component{
 class MainSongSelect extends React.Component{
   constructor(props){
     super(props);
-    
+
   }
   
-
-
+  
   render(){
-    var listItems=this.props.songs.map((song,index) =><li key={song._id}><MusicItem no={index} content={song} setCurrentSong={this.props.setCurrentSong} addToCollection={this.props.addToCollection}/></li>)
+    var listItems=this.props.songs.map((song,index) =>
+      (this.props.display_collection.indexOf(index)>=0 || this.props.display_collection_no===-1)?   //this.props.playing_collection.indexOf(index)>=0?this.props.playing_collection.indexOf(index):
+      (<li key={song._id}><MusicItem no={index} content={song}  display_collection_no={this.props.display_collection_no} current_song_no={this.props.current_song_no} setCurrentSong={this.props.setCurrentSong} addToCollectionShow={this.props.addToCollectionShow} removeFromCollection={this.props.removeFromCollection}/></li>): 
+      ("")
+    )
     return (
-      <div className="col-10 main-song-select">
-        <ul>
+      <div className="col-9 main-song-select">
+
+        <ul className="main-song-select-list">
           {listItems}
+          
+          
         </ul>
+        {this.props.display_collection_no===-1?"":<button className="remove-collection-button"onClick={()=>this.props.removeCollection()}>Remove this collection</button>}
       </div>
     );
   }
@@ -314,27 +613,33 @@ class MusicItem extends React.Component{
   
   changeSong(event){
     //alert("clicked");
-    if(event.target.tagName.toLowerCase()=="div"){
-      this.props.setCurrentSong(this.props.no);
+    if(event.target.tagName.toLowerCase()==="div"){
+      this.props.setCurrentSong(this.props.no,true);
     }
   }
 
   add(){
-    this.props.addToCollection(this.props.no);
+    this.props.addToCollectionShow(this.props.no);
   }
   
+  remove(){
+    this.props.removeFromCollection(this.props.no)
+  }
 
   render(){
 
     return (
-      <div className="music-item" onClick={(e)=>this.changeSong(e)}>
+      <div className={this.props.current_song_no===this.props.no?"music-item selected":"music-item"} onClick={(e)=>this.changeSong(e)}>
         <div className="music-item-info">
           <div className="music-item-title">&ensp;{this.props.content.title}</div>
           <div className="music-item-singer">&ensp;{this.props.content.singer}&ensp;</div>
         </div>
         <div className="music-item-action">
-          <button onClick={()=>this.add()}>Add</button>
+          {this.props.display_collection_no!==-1? <button onClick={()=>this.remove()}>Remove</button>:""}&ensp; 
+          
+          <button onClick={()=>this.add()}>Add</button> &ensp; 
         </div>
+        
         
       </div>
     );
@@ -344,27 +649,50 @@ class MusicItem extends React.Component{
 class AddToCollectionPopUp extends React.Component{
   constructor(props){
     super(props);
+    this.state = {
+      selected: "-"
+    }
     
+  }
+
+  onDropDownListChange(event){
+    
+    this.setState({
+      selected: event.target.value
+    });
   }
 
   backgroundClicked(){
     this.props.addToCollectionCancel();
   }
 
-  render(){
+  addButtonClicked(){
+    if (this.state.selected!=="-"){
+      this.props.addToCollection(this.state.selected);
+    }
+  }
 
+  render(){
+    var listItems=this.props.collections.map((collection,index) =><option value={collection.name} key={index}>{collection.name}</option>)
+    
     return (
       <div>
         <div className="add-to-collection-pop-up-background" onClick={()=>this.backgroundClicked()}/>
         <div className="add-to-collection-pop-up">
           Choose collection:<br/>
-          <select name="dropdown_list"></select>
+          <select name="dropdown_list" onChange={(e)=>this.onDropDownListChange(e)}>
+            <option value="-">-</option>
+            {listItems}
+          </select>
+          <button onClick={()=>this.addButtonClicked()}>Add</button>
         </div>
+        
       </div>
       
     );
   }
 }
+
 
 
 function convertDuration(duration){
